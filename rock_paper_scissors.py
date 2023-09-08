99eb68b7d7a8b87ee0c57686da199aee479c1637
Miles Lambert <mlambert2@cps.edu>
2023-09-08 11:23:07 -0500

    final commit

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,17 +10,33 @@ def get_choices():
     options = ["rock", "paper", "scissors"]
     winner = 0
     while winner == 0:
-    playerC = input("Enter your choice: ")
-    computerC = random.choice(options)
-    choices = {"player": playerC, "computer": computerC}
-    if choices["player"] == "rock" and choices["computer"] == "rock":
-        return "tie"
-    if choices["player"] == "rock" and choices["computer"] == "scissors":
-        return "player wins"
-    if choices["player"] == "rock" and choices["computer"] == "paper":
-        return "computer wins"
-    if choices["player"] == "paper" and choices["computer"] == "paper":
-        return "tie"
+        playerC = input("Enter your choice: ")
+        computerC = random.choice(options)
+        choices = {"player": playerC, "computer": computerC}
+        if choices["player"] == "rock" and choices["computer"] == "rock":
+            return "tie"
+        if choices["player"] == "rock" and choices["computer"] == "scissors":
+            return "player wins"
+            winner += 1
+        if choices["player"] == "rock" and choices["computer"] == "paper":
+            return "computer wins"
+            winner += 1
+        if choices["player"] == "paper" and choices["computer"] == "paper":
+            return "tie"
+        if choices["player"] == "paper" and choices["computer"] == "rock":
+            return "player wins"
+            winner += 1
+        if choices["player"] == "paper" and choices["computer"] == "scissors":
+            return "computer wins"
+            winner += 1
+        if choices["player"] == "scissors" and choices["computer"] == "scissors":
+            return "tie"
+        if choices["player"] == "scissors" and choices["computer"] == "paper":
+            return "player wins"
+            winner += 1
+        if choices["player"] == "scissors" and choices["computer"] == "rock":
+            return "computer wins"
+            winner += 1
     
 
 choices = get_choices()
